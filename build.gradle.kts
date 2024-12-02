d1e66ddf52da713dd2c78509af62cd0d8ff448c2
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2024-12-02 20:26:47 +0000

    added mongodb-springboot-springdata project

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("java-library")
+}
+
+group = "io.flamingock"
+version = "1.0-SNAPSHOT"
+
+
+subprojects {
+    apply(plugin = "java-library")
+    repositories {
+        mavenCentral()
+        mavenLocal()
+    }
+
+    val implementation by configurations
+    val testImplementation by configurations
+    val testRuntimeOnly by configurations
+
+    dependencies {
+
+
+        implementation(kotlin("stdlib-jdk8"))
+        implementation("org.slf4j", "slf4j-api", "2.0.6")
+
+        testImplementation("org.slf4j:slf4j-simple:2.0.6")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+        testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
+        testImplementation(kotlin("test"))
+
+        testImplementation("org.mockito:mockito-core:4.11.0")
+        testImplementation("org.mockito:mockito-junit-jupiter:4.11.0")
+        testImplementation("org.mockito:mockito-inline:4.11.0")
+    }
+    tasks.withType<Test>().configureEach {
+        useJUnitPlatform()
+        testLogging {
+            events(
+                org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+                org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
+                org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+                org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT,
+            )
+        }
+    }
+
+
+    tasks.getByName<Test>("test") {
+        useJUnitPlatform()
+    }
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(8))
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "flamingock-examples"
+include("mongodb-springboot-springdata")

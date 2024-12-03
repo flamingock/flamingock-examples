1cea45a6be3fb511529e4e882ef8a2b22360a76e
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2024-12-03 14:25:04 +0000

    feature: added mongodb-springboot-v3-springdata-v4 example

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,6 @@
 rootProject.name = "flamingock-examples"
 include("mongodb-springboot-springdata")
 include("mongodb-springboot-sync")
+include("mongodb-springboot-v3-springdata-v4")
+
 include("dynamodb-standalone")

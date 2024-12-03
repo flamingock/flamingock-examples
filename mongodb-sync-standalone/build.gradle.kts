5158e63d10067c78139166cf2d9515284a0d84b3
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2024-12-03 17:55:09 +0000

    added mongodb-sync-standalone

---
+++ mongodb-sync-standalone/build.gradle.kts
@@ -0,0 +1,29 @@
+
+val mongodbVersion = "4.3.3"
+val flamingockVersion = project.findProperty("flamingockVersion") as String
+
+dependencies {
+    implementation("io.flamingock:flamingock-core:$flamingockVersion")
+    implementation("io.flamingock:mongodb-sync-v4-driver:$flamingockVersion")
+    implementation("org.mongodb:mongodb-driver-sync:$mongodbVersion")
+    implementation("org.mongodb:mongodb-driver-core:$mongodbVersion")
+    implementation("org.mongodb:bson:$mongodbVersion")
+    implementation("org.slf4j:slf4j-simple:2.0.6")
+
+    testImplementation("org.testcontainers:mongodb:1.18.3")
+    testImplementation("org.testcontainers:junit-jupiter:1.18.3")
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "io.flamingock.examples.community.CommunityStandaloneMongodbSyncApp"
+    }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.compileClasspath)
+    from({
+        configurations.compileClasspath.get().filter {
+            it.name.endsWith("jar")
+        }.map { zipTree(it) }
+    })
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,6 @@ rootProject.name = "flamingock-examples"
 include("mongodb-springboot-springdata")
 include("mongodb-springboot-sync")
 include("mongodb-springboot-v3-springdata-v4")
+include("mongodb-sync-standalone")
 
 include("dynamodb-standalone")

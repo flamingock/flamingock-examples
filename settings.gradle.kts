95cbe9c981647516569783307e995b4f4cb4d3f3
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2025-02-18 16:28:58 +0000

    mongodb modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,24 @@
 
 rootProject.name = "flamingock-examples"
-include("mongodb-springboot-springdata")
-include("mongodb-springboot-sync")
-include("mongodb-springboot-v3-springdata-v4")
-include("mongodb-sync-standalone")
+
+include("mongodb:mongodb-springboot-springdata")
+project(":mongodb:mongodb-springboot-springdata").projectDir = file("mongodb/mongodb-springboot-springdata")
+project(":mongodb:mongodb-springboot-springdata").name = "mongodb-springboot-springdata"
+
+include("mongodb:mongodb-springboot-sync")
+project(":mongodb:mongodb-springboot-sync").projectDir = file("mongodb/mongodb-springboot-sync")
+project(":mongodb:mongodb-springboot-sync").name = "mongodb-springboot-sync"
+
+include("mongodb:mongodb-springboot-v3-springdata-v4")
+project(":mongodb:mongodb-springboot-v3-springdata-v4").projectDir = file("mongodb/mongodb-springboot-v3-springdata-v4")
+project(":mongodb:mongodb-springboot-v3-springdata-v4").name = "mongodb-springboot-v3-springdata-v4"
+
+include("mongodb:mongodb-sync-standalone")
+project(":mongodb:mongodb-sync-standalone").projectDir = file("mongodb/mongodb-sync-standalone")
+project(":mongodb:mongodb-sync-standalone").name = "mongodb-sync-standalone"
+
+
+
 
 include("mysql-springboot")
 include("mysql-standalone")
